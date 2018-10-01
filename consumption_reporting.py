3d92b2245fbce24234d9dd381518935d6f5bf9db
Bastien Mazeran <15131317+mazerab@users.noreply.github.com>
2018-10-01 15:40:10 -0700

    final script

--- consumption_reporting.py
+++ consumption_reporting.py
@@ -1,7 +1,15 @@
 import argparse
 import json
+import polling
 import requests
 import urllib
+
+def downloadCsvFile(download_url, download_filename):
+    print 'Downloading CSV file ...'
+    resp = requests.get(download_url)
+    resp.raise_for_status()
+    if resp.status_code == 200:
+        open(download_filename, 'wb').write(resp.content)
 
 def getContracts(access_token):
     print 'Getting Contracts ...'
@@ -10,42 +18,56 @@ def getContracts(access_token):
     resp = requests.get(get_contract_url, headers=headers)
     resp.raise_for_status()
     if resp.status_code == 200:
+        return resp.json()
+
+def getExportRequestsDetails(access_token, contract_number, request_key):
+    print 'Getting Export Request Details ...'
+    export_results_url = base_tokenflex_api + '/v1/export/' + contract_number + '/requests/' + urllib.quote_plus(request_key)
+    headers = {'Authorization': 'Bearer ' + access_token}
+    resp = requests.get(export_results_url, headers=headers)
+    resp.raise_for_status()
+    if resp.status_code == 200:
         print resp.text
         return resp.json()
 
+def pollExportRequestDetails(access_token, contract_number, request_key):
+    print 'Polling Export Request Details ...'
+    export_results_url = base_tokenflex_api + '/v1/export/' + contract_number + '/requests/' + urllib.quote_plus(request_key)
+    headers = {'Authorization': 'Bearer ' + access_token}
+    web_handle = polling.poll(
+        lambda: requests.get(export_results_url, headers=headers).json()['requestStatus'] == 'Download',
+        step = 5,
+        timeout = 120
+    )
+    return web_handle
+
 # POST /export/:contract_number/requests
 def submitExportRequest(access_token, contract_number):
-    print 'Requesting export for contract: ' + contract_number
+    print 'Submitting export for contract: ' + contract_number
     export_request_url = base_tokenflex_api + '/v1/export/' + contract_number + '/requests'
     headers = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token }
     payload = {
         "fields": [
             "contractYear",
-            "chargedItemID",
             "tokenPool",
-            "reasonType",
-            "reasonComment",
             "usageCategory",
             "usageDate",
-            "transactionDate",
             "productLineCode",
             "productName"
         ],
         "metrics": [
-            "tokensConsumed",
-            "quantity"
+            "tokensConsumed"
         ],
         "usageCategory": [
-            "MANUAL_ADJUSTMENT",
-            "MANUAL_CONSUMPTION"
+            "DESKTOP_PRODUCT",
+            "CLOUD_PRODUCT"
         ],
         "where": "contractYear=1",
-        "downloadFileName": "myYear1Adjustments.csv"
+        "downloadFileName": contract_number + "_myYear1DesktopCloudUsage.csv"
     }
     resp = requests.post(export_request_url, headers=headers, data=json.dumps(payload))
     resp.raise_for_status()
     if resp.status_code == 200:
-        print resp.text
         return resp.json()
 
 parser = argparse.ArgumentParser(description='Run consumption report.')
@@ -78,6 +100,24 @@ while accepted.lower() == 'n':
             contracts = getContracts(access_token)
             # Step 3: Submit an Export request
             for contract in contracts:
+                print '*** Found contract: ' + contract['contractNumber']
                 contract_number = contract['contractNumber']
-                request = submitExportRequest(access_token, contract_number)
+                export_request = submitExportRequest(access_token, contract_number)
                 # Step 4: Poll for request results
+                request_key = export_request['requestKey']
+                print '*** Submitted export request: ' + request_key
+                request_details = getExportRequestsDetails(access_token, contract_number, request_key)
+                if request_details == 'Download':
+                    request_status = request_details['requestStatus']
+                    print '*** Retrieved export status: ' + request_status
+                else:
+                    poll_status = pollExportRequestDetails(access_token, contract_number, request_key)
+                    if poll_status == True:
+                        request_details = getExportRequestsDetails(access_token, contract_number, request_key)
+                        request_status = request_details['requestStatus']
+                        print '*** Retrieved export status: ' + request_status
+                        download_url = request_details['downloadUrl']
+                        print '*** Download url: ' + download_url
+                        download_filename = request_details['downloadFileName']
+                        downloadCsvFile(download_url, download_filename)
+                        print '*** Downloaded file: ' + download_filename
